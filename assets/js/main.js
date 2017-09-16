a715344e20a1361e28c3fd4c0ce911310766a328
Jason Li <dev.jasonli@gmail.com>
2017-09-16 08:18:53 +0000

    production final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,9 @@
 $(document).ready(function() {
-    window.sr = ScrollReveal({ reset: true });
+    window.sr = ScrollReveal({ reset: true, vFactor: 0.0 });
     sr.reveal('hr');
-    sr.reveal('.col-12');
     sr.reveal('.row');
 });
 
-// scrolling //
-
-$('a[href*=\\#]').on('click', function(event) {
-    event.preventDefault();
-    $('html,body').animate({ scrollTop: $(this.hash).offset().top }, easeOutSine, 500);
-});
 
 $(window).scroll(function() {
     if ($(this).scrollTop() > 50) {
