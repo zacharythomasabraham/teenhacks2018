78f5b8327ea4516075be81306c6d2c7a14449da3
Jason Li <dev.jasonli@gmail.com>
2017-09-17 08:01:10 +0000

    smoooth scroll, added subscribe form

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,22 @@ $(document).ready(function() {
     window.sr = ScrollReveal({ reset: true, vFactor: 0.0 });
     sr.reveal('hr');
     sr.reveal('.row');
+
+    particlesJS.load('particles-js', '/assets/json/particles.json', function() {
+        console.log('particles.js loaded');
+    });
+
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 387.5, 'easeOutSine', function() {
+                window.location.hash = hash;
+            });
+        }
+    });
 });
 
 
